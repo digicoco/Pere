04121e9f3d5e4a90542ec11db5730ae80a021009
digicoco <digicoco@amazon.com>
2026-06-17 07:29:56 -0700

    copy: 'Review all 12 badge photos'

--- src/components/MultiPromptPanel.tsx
+++ src/components/MultiPromptPanel.tsx
@@ -159,7 +159,7 @@ export const MultiPromptPanel = ({ item, subPrompts, onApproveAll, onApproveOne,
           <Col gridGap="dimensions.spacing.200">
             <ActionLink onClick={() => onOpenPhotoGrid?.()}>
               <Text fontSize="T200" fontWeight="medium" color="color.neutral.90">
-                View all {subPrompts.length} badge photos
+                Review all {subPrompts.length} badge photos
               </Text>
               <IconChevronRightSmall aria-hidden="true" />
             </ActionLink>
